3d73b31815048cd9b18f4092c46dee73fc455082
SeymaNurAksoy <aksoyseyma192@gmail.com>
2025-06-11 22:31:38 +0300

    commit

--- cypress/e2e/cypress-training/backForward.cy.js
+++ cypress/e2e/cypress-training/backForward.cy.js
@@ -1,3 +1,16 @@
 describe('Back Forward for cypress', () => {
-    
+    it('cypress.io sayfaina gidip tarayicida ileri geri komutlarini ogrenecegiz', () => {
+        cy.visit('https://www.cypress.io');
+        cy.get(':nth-child(5) > .nav-bar-link').click();
+        cy.title().should('eq', 'Scale Open-Source Testing with Cypress Cloud | Cypress Cloud Pricing');
+        cy.wait(1000);
+        cy.go(-1); // Tarayıcı geri git
+        cy.title().should('eq', 'Testing Frameworks for Javascript | Write, Run, Debug | Cypress');
+        cy.wait(1000);
+        cy.go(1); // Tarayıcı ileri git
+        cy.title().should('eq', 'Scale Open-Source Testing with Cypress Cloud | Cypress Cloud Pricing');
+
+        
+        
+    });
 });
